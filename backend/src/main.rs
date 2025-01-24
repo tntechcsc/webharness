9889cad264d0d5782e2dfe1b41699946ce184b5b
chromody <chromody@gmail.com>
2025-01-24 16:17:55 -0600

    added session checking functions
    Added a function that checks the validity of a session, which simply checks if the time difference of now and  the end time is greater than 1 hour. If it is, then the function returns false, otherwise it returns true.
    
    Also added a function named has_excess_sessions which checks if there are 1 or more existing sessions. For now I assume a user only needs one session, so if the user has 1 or more sessions the function returns true. Otherwise it returns false.
    
    Also added a function to delete all sessions for a given user id, which uses a query_map to iterate through all the rows of sessions.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,7 +18,7 @@ use rocket::State;
 use winapi::um::processthreadsapi::OpenProcess;
 use winapi::um::winnt::PROCESS_TERMINATE;
 use winapi::um::processthreadsapi::TerminateProcess;
-use chrono::{DateTime, Utc, Duration};
+use chrono::{DateTime, Utc, Duration, TimeDelta};
 
 struct ProcessInfo {
     pid: u32,
@@ -206,7 +206,7 @@ struct Session {
     #[schema(example = "ajhniksdjnkdsa-7e1f-4686-bc90-b010ff98213e")]
     id: String,
     #[schema(example = "95dcd4e0-7e1f-4686-bc90-b010ff98213e")]
-    username: String,
+    userId: String,
     #[schema(example = "START TIME")]
     startTime: String,
     #[schema(example = "END TIME")]
@@ -273,8 +273,98 @@ fn user_password_check(username: &String, password: &String, conn: &std::sync::M
     }
 }
 
-fn is_session(userId, conn: &std::sync::MutexGuard<'_, rusqlite::Connection>) -> bool {
+fn session_valid(userId: String, conn: &std::sync::MutexGuard<'_, rusqlite::Connection>) -> bool {
+    let endTimeString: String;
+    let endTime: DateTime<Utc>;
+    let now: DateTime<Utc> = Utc::now();
+    let delta: TimeDelta;
+    let hour: TimeDelta = TimeDelta::hours(1);
     
+    let mut stmt = conn.prepare("SELECT endTime FROM Session WHERE userId = ?1").unwrap();
+    let mut result = stmt.query(&[&userId]).unwrap();
+
+    match result.next() {
+        Ok(Some(unwrapped_row)) => {
+            // If a user is found
+            endTimeString = unwrapped_row.get(0).unwrap();
+        }
+        Ok(None) => {
+            return false;
+        }
+        Err(_) => {
+            // Querying error, return 500 Internal Server Error
+            return false;
+        }
+    }
+
+    endTime = endTimeString.parse().unwrap();
+    delta = now - endTime; 
+    if delta >= hour {
+        return false
+    }
+    else {
+        return true;
+    }
+}
+
+fn has_excess_sessions(userId: String, conn: &std::sync::MutexGuard<'_, rusqlite::Connection>) -> bool {
+    let mut stmt = conn.prepare("SELECT COUNT(*) FROM Session WHERE userId = ?1").unwrap(); // Prepare your query
+    let mut result = stmt.query(&[&userId]).unwrap(); // Execute the query
+    let mut count = 0;
+    if let Some(row) = result.next().unwrap() { // Unwrap the first row
+        count = row.get(0).unwrap(); // Get the first column (COUNT(*) result)
+    }
+    if count >= 1 {
+        return true;
+    }
+    else {
+        return false;
+    }
+    /*
+    match rows.next() { // Use match to handle the Option returned by next()
+        Ok(Some(unwrapped_row)) => { // If there is a row
+            let user_name: String = unwrapped_row.get(0).unwrap();
+            let user_age: i32 = unwrapped_row.get(1).unwrap();
+            return 201;
+        }
+        Ok(None) => { // If no rows were returned
+            return 404;
+        }
+        Err(_) => { // Handle any potential errors from querying
+            return 500;
+        }
+    }
+    */
+}
+
+//function to delete every session for a given user id
+fn delete_session(userId: String, conn: &std::sync::MutexGuard<'_, rusqlite::Connection>) -> () {
+    let mut id: String; // the ids of the session we want to delete
+
+    // preparing a statement to collect all session ids for the given user id
+    let mut stmt = conn.prepare("SELECT id from Session WHERE userId = ?1").unwrap();
+    let mut rows = stmt.query_map([&userId], |row| { //creating a query map with the expected rows of Session
+        Ok(Session {
+            id: row.get(0).expect("ERROR"), // we only need ids
+            userId: "".to_string(),
+            endTime: "".to_string(),
+            startTime: "".to_string(),
+            })
+        }
+    ).unwrap();
+
+    let mut ids: Vec<String> = Vec::new(); // a vector to hold our ids
+     
+    for row in rows { // iterating through the query map
+        let session = row.unwrap(); // unwrapping a session from the query map
+        ids.push(session.id) // pushing it into the ids vector
+    }
+
+    for id in ids.iter_mut() { // iterating through the ids vector to delete every session with the session ids we collected
+        let query = "DELETE FROM Session WHERE id = ?1"; // making a query to delete that session id
+        let result = conn.execute(query, &[&id.to_string()]); // executing that query
+    }
+
 }
 
 #[utoipa::path(
@@ -408,6 +498,10 @@ fn user_login(user_data: Json<Login>, db: &rocket::State<Arc<DB>>) -> Result<Jso
             // Querying error, return 500 Internal Server Error
             return Err(Status::InternalServerError);
         }
+    }
+
+    if session_valid(userId.clone(), &conn) == false || has_excess_sessions(userId.clone(), &conn) {
+        delete_session(userId.clone(), &conn);
     }
 
     let query = "INSERT INTO Session (id, userId, startTime, endTime) VALUES (?1, ?2, ?3, ?4)";
