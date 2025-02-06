60e3f260f188337b4d46e54aca51f3187d22735d
chromody <chromody@gmail.com>
2025-02-06 17:08:11 -0600

    added protected routes capabilities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import  ProtectedRoute  from "./ProtectedRoute"
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from './pages/home';
 import Login from './pages/login';
 import './App.css';
@@ -10,7 +11,7 @@ function App() {
       <div className="d-flex flex-column min-vh-100 bg-dark text-light">
         <div className="flex-grow-1">
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<ProtectedRoute element={<Home />} />} />
             <Route path="/login" element={<Login />} />
           </Routes>
         </div>
---
+++ frontend/src/ProtectedRoute.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { checkSession } from "./CheckSession"
+
+// Protected route component
+const ProtectedRoute = ({ element }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+  
+    useEffect(() => {
+      const validateSession = async () => {
+        const isValid = await checkSession();
+        setIsAuthenticated(isValid);
+      };
+  
+      validateSession();
+    }, []);
+  
+    if (isAuthenticated === null) {
+      return <div>Loading...</div>; // Loading state while checking session
+    }
+  
+    // If the user is not authenticated, redirect to login page
+    if (!isAuthenticated) {
+      return <Navigate to="/login" />;
+    }
+  
+    return element; // Show the protected element
+  };
+
+  export default ProtectedRoute;
