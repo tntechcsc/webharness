4db4578c2857e42ca129cd2c4bf049e5fe21dbba
chromody <chromody@gmail.com>
2025-02-18 20:15:54 -0600

    added comment

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
 import Navbar from '../components/Navbar';
-import "bootstrap/dist/css/bootstrap.min.css";
+
+//maybe use bootstrap instead of all this css
 
 const Layout = ({ children, title }) => {
   // These could be moved to a context or auth provider later
