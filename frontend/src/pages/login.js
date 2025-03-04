9baf35123fd7fe01db937c44ab4a700fab7081bf
FireNation5 <etandunlap@gmail.com>
2025-03-04 15:11:11 -0600

    Added password hide functionalty to login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -3,12 +3,21 @@ import axios from "axios";
 import "./login.css"; // Assuming you save your CSS in Login.css
 import Button from "@mui/material/Button";
 import Alert from '@mui/material/Alert';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 
 const Login = () => {
 
   const [loginSuccess, setLoginSuccess] = React.useState("");
   const [loginError, setLoginError] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -21,20 +30,15 @@ const Login = () => {
     axios.post(`${baseURL}:3000/api/user/login`, { username, password })
       .then(response => {
         console.log("Login successful:", response.data);
-        // insert received session token into div #token
-        //document.getElementById("token").innerText = JSON.stringify(response.data.session_id);
-        // store session token in sessionStorage
         sessionStorage.setItem("session_id", response.data.session_id);
         setLoginSuccess(true);
         setLoginError("");
-        // Handle success by redirecting to /
         window.location.href = "/";
       })
       .catch(error => {
         console.error("Login error:", error);
         setLoginError("Login Failed. Please check your username and password.");
         setLoginSuccess(false);
-        // Handle error (e.g., display error message)
       });
   };
 
@@ -45,7 +49,15 @@ const Login = () => {
         <div style={{ fontSize: '32px', color: '#6ffb78' }}>Mangrove</div>
         <div className="d-flex flex-column align-items-center">
           <input className="input mb-3" name="username" placeholder="Username" type="username" required />
-          <input className="input mb-3" name="password" placeholder="Password" type="password" required />
+          <div className="input-group mb-3 position-relative">
+            <input className="input form-control" name="password" placeholder="Password" type={showPassword ? "text" : "password"} required />
+            <div className="position-absolute top-50 end-0 translate-middle-y pe-0">
+              <FormControlLabel
+                control={<Checkbox icon={<VisibilityOff />} checkedIcon={<Visibility />} onChange={togglePasswordVisibility} />}
+                label=""
+              />
+            </div>
+          </div>
           <button type="submit" className="button">Log In →</button>
           {loginError && <Alert variant="filled" severity="error" style={{ marginTop: '20px' }}>{loginError}</Alert>}
           {loginSuccess && <Alert variant="filled" severity="success" style={{ marginTop: '20px' }}>Login Successful!</Alert>}
