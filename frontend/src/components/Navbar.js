2549bc47faddcfcff499d65e1053b3c0302e5389
t-mlyvanh <tommymalyvanh@gmail.com>
2025-03-04 15:11:30 -0600

    Double Import

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, IconButton } from "@mui/material";
 import HomeIcon from '@mui/icons-material/Home';
